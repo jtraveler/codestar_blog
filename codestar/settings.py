bfd82ef9acdb85b3eb4f4da1d1af1efd5d942a24
jtraveler <matthew.jtraveler@gmail.com>
2025-06-11 03:46:54 +0200

    Configure environment variables for SECRET_KEY and DATABASE_URL

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o$w!+s8m!g1v#vq39e^kpzes&e^!)ql13uuz@#$q)e&6-l%w#0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_VgGFeEvSp9C0@ep-cool-thunder-a2celn2y.eu-central-1.aws.neon.tech/dart_boxer_bribe_789768")
+os.environ.setdefault("SECRET_KEY", "django-insecure-o$w!+s8m!g1v#vq39e^kpzes&e^!)ql13uuz@#$q)e&6-l%w#0")
