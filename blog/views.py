cf701cbb04618d4c190a4e1eb62a818fff4b8015
jtraveler <matthew.jtraveler@gmail.com>
2025-06-13 08:42:20 +0200

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+
 
 class PostList(generic.ListView):
     model = Post
@@ -10,3 +11,25 @@ class PostList(generic.ListView):
     
     def get_queryset(self):
         return Post.objects.filter(status=1).order_by('-created_on')
+    
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
