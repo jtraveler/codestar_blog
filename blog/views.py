124d960d6440fc3428d1a67d796f00a8dd308909
jtraveler <matthew.jtraveler@gmail.com>
2025-06-17 00:00:25 +0200

    Display a confirmation message

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
-
+# Create your views here.
 class PostList(generic.ListView):
-    model = Post
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-    
-    def get_queryset(self):
-        return Post.objects.filter(status=1).order_by('-created_on')
-    
+
 
 def post_detail(request, slug):
     """
@@ -21,21 +19,30 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
-    ``comments``
-        All approved comments related to the post.
 
     **Template:**
 
     :template:`blog/post_detail.html`
     """
+
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    
-    # Get all approved comments for this post
-    comments = post.comments.filter(approved=True).order_by('created_on')
-    comment_count = comments.count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -44,29 +51,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
-            "coder": "Matt Rudge"
+            "comment_form": comment_form,
         },
-    )
-
-
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post,
-         "coder": "Matt Rudge"},
     )
